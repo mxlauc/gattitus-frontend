73ebd510d9a9c1801d38e3302cb65c55e9e8bd02
Amilcar Laura <denotefull@gmail.com>
2022-04-08 13:28:35 -0500

    Aceptar url de imagen en imagePreloader

--- src/components/ImagePreloader.vue
+++ src/components/ImagePreloader.vue
@@ -2,10 +2,10 @@
     <div
         class="overflow-hidden"
         style="display:inline-table; max-width:100%"
-        :style="{aspectRatio: aspectRatio, background: `linear-gradient(45deg, ${image?.meta_data.color_bl} 0%, ${image?.meta_data.color_tr} 100%)`}">
+        :style="{aspectRatio: aspectRatio, background: `linear-gradient(45deg, ${image?.meta_data?.color_bl} 0%, ${image?.meta_data?.color_tr} 100%)`}">
         <img
             class="image-preloader img-fluid w-100 opacity-0"
-            :src="image?.[option ?? 'url_sm']"
+            :src="image?.[option ?? 'url_sm'] ?? image"
             :style="{aspectRatio: aspectRatio}"
             @load="onLoadImage">
     </div>
@@ -23,10 +23,10 @@ export default {
             if (this.aspect) {
                 return this.aspect
             }
-            if (this.image?.meta_data.aspect_ratio < 0.9) {
+            if (this.image?.meta_data?.aspect_ratio < 0.9) {
                 return 0.9
             }
-            return this.image?.meta_data.aspect_ratio
+            return this.image?.meta_data?.aspect_ratio ?? 1
         },
     },
 }
