8dd188cbf7567bc450a870b56d01077b495a27fe
Amilcar Laura <denotefull@gmail.com>
2022-04-25 15:20:54 -0500

    Agregar items de ejemplo a menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -72,6 +72,82 @@
                                         <span>{{ $t('profile') }}</span>
                                     </li>
                                 </router-link>
+
+                                <router-link :to="mainStore.userLogged ? ('/@' + mainStore.userLogged.username) : '/'">
+                                    <li>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="25"
+                                            height="25"
+                                            fill="currentColor"
+                                            class="bi bi-person-fill"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                        </svg>
+                                        <span>{{ $t('profile') }}</span>
+                                    </li>
+                                </router-link>
+                                <router-link :to="mainStore.userLogged ? ('/@' + mainStore.userLogged.username) : '/'">
+                                    <li>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="25"
+                                            height="25"
+                                            fill="currentColor"
+                                            class="bi bi-person-fill"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                        </svg>
+                                        <span>{{ $t('profile') }}</span>
+                                    </li>
+                                </router-link>
+                                <router-link :to="mainStore.userLogged ? ('/@' + mainStore.userLogged.username) : '/'">
+                                    <li>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="25"
+                                            height="25"
+                                            fill="currentColor"
+                                            class="bi bi-person-fill"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                        </svg>
+                                        <span>{{ $t('profile') }}</span>
+                                    </li>
+                                </router-link>
+                                <router-link :to="mainStore.userLogged ? ('/@' + mainStore.userLogged.username) : '/'">
+                                    <li>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="25"
+                                            height="25"
+                                            fill="currentColor"
+                                            class="bi bi-person-fill"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                        </svg>
+                                        <span>{{ $t('profile') }}</span>
+                                    </li>
+                                </router-link>
+                                <router-link :to="mainStore.userLogged ? ('/@' + mainStore.userLogged.username) : '/'">
+                                    <li>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="25"
+                                            height="25"
+                                            fill="currentColor"
+                                            class="bi bi-person-fill"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                        </svg>
+                                        <span>{{ $t('profile') }}</span>
+                                    </li>
+                                </router-link>
                             </ul>
                         </div>
                     </div>
