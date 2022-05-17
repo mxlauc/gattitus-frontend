6d0cede52eb6d3b3b9011909e7783d4baf7c4bfc
Amilcar Laura <denotefull@gmail.com>
2022-05-17 16:28:42 -0500

    agregar pagina de error 404

---
+++ pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        404
+        <br>
+        {{ error }}
+        <br>
+        <NuxtLink
+            to="/"
+            class="text-decoration-none fw-bold text-black d-inline-block">
+            volver home
+        </NuxtLink>
+    </div>
+</template>
+<script>
+definePageMeta({
+    layout: "main",
+})
+export default {
+    props: ["error"]
+}
+</script>
