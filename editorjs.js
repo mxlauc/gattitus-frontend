85d72e7d9e0671abba1b04d1edafac8cfd089401
Amilcar Laura <denotefull@gmail.com>
2022-05-18 14:51:03 -0500

    Eliminar archivos js innecesarios

--- editorjs.js
+++
@@ -1,8 +0,0 @@
-let obj = Object
-
-if (process.client) {
-    const m = await import("@editorjs/editorjs")
-    obj = m.default
-}
-
-export default obj
--- imageTool.js
+++
@@ -1,8 +0,0 @@
-let obj = Object
-
-if (process.client) {
-    const m = await import("@editorjs/image")
-    obj = m.default
-}
-
-export default obj
