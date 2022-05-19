9bf5587e97f22934461c8cff5f49b519c2668961
Amilcar Laura <denotefull@gmail.com>
2022-05-19 10:51:19 -0500

    Actualizar iconos

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,4 @@
 self.addEventListener("fetch", function (event) {
-    console.log(event.request.url)
     if (event.request.mode === "navigate") {
         event.respondWith((async () => {
             try {
