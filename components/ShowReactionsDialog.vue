b40a6c0f45b7c863893608c2efc0d10afc91b40e
Amilcar Laura <denotefull@gmail.com>
2022-05-23 10:45:04 -0500

    Aplicar scroll a ver reacciones

--- components/ShowReactionsDialog.vue
+++ components/ShowReactionsDialog.vue
@@ -6,36 +6,44 @@
             Reacciones
         </template>
         <div
-            style="max-height:80vh; overflow-y: auto;"
-            class="custom-scroll"
-            ref="scroller">
-            <div
-                v-for="r in reactions"
-                :key="r.id"
-                class="my-1">
-                <NuxtLink
-                    :to="r.user.url"
-                    role="button">
-                    <ImagePreloader
-                        :image="r.user.image"
-                        class="shadow rounded-5"
-                        :aspect="1"
-                        style="width: 40px;" />
-                </NuxtLink>
-
-                <NuxtLink
-                    :to="r.user.url"
-                    class="text-decoration-none text-dark ms-2"
-                    role="button">
-                    {{ r.user.name }}
-                </NuxtLink>
-            </div>
+            class="v-simple-infinite-scroll-container custom-scroll"
+            style="max-height:60vh; overflow-y: auto; overflow-x: hidden;">
+            <VSimpleInfiniteScroll
+                @load="loadMore">
+                <div
+                    v-for="r in reactions"
+                    :key="r.id"
+                    class="my-2">
+                    <div class="row g-2">
+                        <div class="col-auto">
+                            <NuxtLink
+                                :to="r.user.url"
+                                role="button">
+                                <ImagePreloader
+                                    :image="r.user.image"
+                                    class="shadow rounded-5"
+                                    :aspect="1"
+                                    style="width: 40px;" />
+                            </NuxtLink>
+                        </div>
+                        <div class="col">
+                            <NuxtLink
+                                :to="r.user.url"
+                                class="text-decoration-none text-dark"
+                                role="button">
+                                {{ r.user.name }}
+                            </NuxtLink>
+                        </div>
+                    </div>
+                </div>
+            </VSimpleInfiniteScroll>
         </div>
     </CustomModal>
 </template>
 <script>
 import CustomModal from "~/components/CustomModal.vue"
 import ImagePreloader from "~/components/images/ImagePreloader.vue"
+import VSimpleInfiniteScroll from "./VSimpleInfiniteScroll.vue"
 import axios from "axios"
 import { useMainStore } from "~/store/mainStore"
 
@@ -43,6 +51,7 @@ export default {
     components: {
         CustomModal,
         ImagePreloader,
+        VSimpleInfiniteScroll,
     },
     setup () {
         const mainStore = useMainStore()
@@ -54,53 +63,33 @@ export default {
         return {
             reactions: [],
             reactionsPaginator: null,
-            loadingMore: true,
         }
     },
     props: ["url"],
     emits: ["close"],
     mounted () {
         this.getReactions(this.url)
-        this.addScrollListener()
-    },
-    beforeUnmount () {
-        this.removeScrollListener()
     },
     methods: {
-        addScrollListener () {
-            this.$refs.scroller.addEventListener("scroll", this.onScroll)
-        },
-        removeScrollListener () {
-            this.$refs.scroller.removeEventListener("scroll", this.onScroll)
-        },
-        onScroll () {
-            const pixelsToBottom = this.$refs.scroller.scrollHeight - (this.$refs.scroller.clientHeight + this.$refs.scroller.scrollTop)
-            if (pixelsToBottom < 200) {
-                this.loadMore()
-            }
-        },
-        loadMore () {
-            if (this.loadingMore) {
+        loadMore (scroll) {
+            if (!this.reactionsPaginator) {
+                scroll.loaded()
                 return
             }
-            this.loadingMore = true
-
             if (!this.reactionsPaginator.next) {
-                console.log("ya no hay mas, desactivar listener")
-                this.removeScrollListener()
-                this.loadingMore = false
+                scroll.complete()
                 return
             }
-
-            console.log("cargando mas...")
             this.getReactions(this.reactionsPaginator.next)
+                .then(() => {
+                    scroll.loaded()
+                })
         },
         getReactions (url) {
-            axios.get(`${url}`)
+            return axios.get(`${url}`)
                 .then(response => {
                     this.reactionsPaginator = response.data.links
                     this.reactions = this.reactions.concat(response.data.data)
-                    this.loadingMore = false
                 })
         }
     }
